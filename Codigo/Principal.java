bc122a01f0eda59c880782421da40b1abab18204
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2022-03-31 10:56:02 -0300

    .

--- Codigo/Principal.java
+++ Codigo/Principal.java
@@ -2,7 +2,7 @@ package Codigo;
 
 public class Principal{
 
-    public void main(String args[]){
+    public static void main(String args[]){
         Simulacao sim = new Simulacao();
         sim.executarSimulacao(40);
     }
